c739c0daaa91810047ab0ecef774496ba6c1a020
Sunil <sunilthedj@gmail.com>
2024-08-02 11:50:14 +0530

    ARM64 Arch Support

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Building Plugin..."
+echo "HOST Arch: $(uname -m)"
 
 PLUGIN_DIR="./plugins"
 # Navigate to the plugins directory
@@ -13,7 +14,12 @@ for DIR in */ ; do
         # Navigate into the directory
         cd "$DIR"
         # Build the plugin, naming the output file based on the directory name
-        docker run -it -v "$PWD:/app" -w /app krakend/builder:2.7.0 go build -buildmode=plugin -o "${DIR%/}.so" .
+        # if host is arm64
+        if [ "$(uname -m)" == "arm64" ]; then
+            docker run -it -v "$PWD:/app" -w /app -e "CGO_ENABLED=1" -e "CC=aarch64-linux-musl-gcc" -e "GOARCH=arm64" -e "GOHOSTARCH=amd64" krakend/builder:2.7.0 go build -buildmode=plugin -o "${DIR%/}.so" .
+        else
+            docker run -it -v "$PWD:/app" -w /app krakend/builder:2.7.0 go build -buildmode=plugin -o "${DIR%/}.so" .
+        fi
         # Move the .so file to the plugins directory
         mv "${DIR%/}.so" ../
         # Navigate back to the plugins directory
@@ -22,3 +28,4 @@ for DIR in */ ; do
 done
 
 echo "Plugin build process completed."
+echo "RUN docker compose up to start the server"
